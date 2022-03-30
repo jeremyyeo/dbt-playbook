7acabc1c1a4a83d4b6105e6667b3fd21a9072eeb
jeremyyeo <jeremyyeo@gmail.com>
2022-03-30 21:21:21 +1300

    update

--- src/dbt-is-working/tests/test_playbook.py
+++ src/dbt-is-working/tests/test_playbook.py
@@ -5,7 +5,9 @@ tests_path = Path(__file__).parent.resolve()
 
 
 def test_debug():
-    assert os.system("dbt --help") == 0
-    # f = open(f"{tests_path}/text.txt")
-    # assert f.readline() == "hello"
-    # assert Path(__file__).parent.resolve() == "test"
+    assert (
+        os.system(
+            f"dbt debug --profiles-dir {tests_path}/my_dbt_project --project-dir {tests_path}/my_dbt_project"
+        )
+        == 0
+    )
