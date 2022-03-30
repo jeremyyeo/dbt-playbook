60ad364b4052589f9d9939563591544e47763fce
jeremyyeo <jeremyyeo@gmail.com>
2022-03-30 21:43:03 +1300

    move profiles.yml

--- src/dbt-is-working/tests/test_playbook.py
+++ src/dbt-is-working/tests/test_playbook.py
@@ -2,12 +2,13 @@ import os
 from pathlib import Path
 
 tests_path = Path(__file__).parent.resolve()
+cwd = os.getcwd()
 
 
 def test_debug():
     assert (
         os.system(
-            f"dbt debug --profiles-dir {tests_path}/my_dbt_project --project-dir {tests_path}/my_dbt_project"
+            f"dbt debug --profiles-dir {cwd}/.dbt --project-dir {tests_path}/my_dbt_project"
         )
         == 0
     )
